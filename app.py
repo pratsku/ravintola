4d5816d51c105a44c256c0b9f5f4e1f493fecda6
Pratham Narsingoju <pr8th8m@Prathams-MacBook-Air.local>
2026-01-29 15:33:32 +0200

    Fix Flask app initialization (__name__)

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
+import sqlite3
 from flask import Flask
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import db
+
 
 app = Flask("__name__")
 
 @app.route("/")
 def index():
-    return "Hell0!"
+    return "Tähän tulee ravintolahaku"
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
