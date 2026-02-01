785c6d45d472ff860feeb1db3847bf768391ed9c
Pratham Narsingoju <pr8th8m@Prathams-MacBook-Air.local>
2026-02-01 02:18:37 +0200

    Show item list/info

--- items.py
+++ items.py
@@ -4,3 +4,16 @@ def add_item(title, description, start_price, user_id):
     sql = """INSERT INTO items (title, description, start_price, user_id)
             VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, description, start_price, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.description
+                    items.start_price
+                    users.username
+            FROM items, users
+            WHERE items.user_id = users.id AND items.id = ?"""
+    return db.query(sql, [item_id])[0]
