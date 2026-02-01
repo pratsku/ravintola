9bdbe2bfe52351057149da62e666e67824cfdd21
Pratham Narsingoju <pr8th8m@Prathams-MacBook-Air.local>
2026-02-01 16:33:40 +0200

    Add not found handling

--- items.py
+++ items.py
@@ -20,7 +20,8 @@ def get_item(item_id):
             FROM items
             JOIN users ON items.user_id = users.id
             WHERE items.id = ?"""
-    return db.query(sql, [item_id])[0]
+    result = db.query(sql, [item_id])
+    return result[0] if result else None
 
 def update_item(item_id, title, description):
     sql = """UPDATE items SET title = ?,
