0baa36c39bf33336521d4688aa6fa4caf2ae4f8a
Pratham Narsingoju <pr8th8m@Prathams-MacBook-Air.local>
2026-02-01 15:47:35 +0200

    Add find item feature

--- items.py
+++ items.py
@@ -32,3 +32,11 @@ def update_item(item_id, title, description):
 def remove_item(item_id):
     sql = "DELETE FROM items WHERE id = ?"
     db.execute(sql, [item_id])
+
+def find_item(query):
+    sql = """SELECT id, title
+            FROM items
+            WHERE title LIKE ? OR description LIKE ?
+            ORDER BY id DESC"""
+    like = "%" + query + "%"
+    return db.query(sql, [like, like])
